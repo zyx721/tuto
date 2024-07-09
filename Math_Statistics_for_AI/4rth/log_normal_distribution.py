bfc8a001077aeee896ba8e861ad0859c56bbf36c
zyx721 <benmatiziad5@gmail.com>
2024-07-09 18:29:02 +0100

    update

--- Math_Statistics_for_AI/4rth/log_normal_distribution.py
+++ Math_Statistics_for_AI/4rth/log_normal_distribution.py
@@ -1,6 +1,20 @@
 import matplotlib.pyplot as plt
-import pandas as np
-df = np.read_csv("income.csv")
+import pandas as pd
+import seaborn as sns
+
+# Read the CSV file
+df = pd.read_csv("income.csv")
 print(df)
-df.plot(x='income($)',y='count')
+
+# Plot with logarithmic x-axis
+
+
+
+# Set seaborn plot size
+sns.set(rc={'figure.figsize':(11.7, 8.27)})
+
+# Create a bar plot
+g = sns.barplot(x='income($)', y='count', data=df)
+g.set_xticklabels(g.get_xticklabels(), rotation=45, horizontalalignment='right')
+g.set(xscale="log")
 plt.show()
