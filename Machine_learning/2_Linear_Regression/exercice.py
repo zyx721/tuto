466ab86b31a47b729a901796c1376d1510bf4fc6
zyx721 <benmatiziad5@gmail.com>
2024-07-21 09:57:27 +0100

    update

---
+++ Machine_learning/2_Linear_Regression/exercice.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+df = pd.read_csv("canada_per_capita_income.csv")
+print(df)
+plt.xlabel("year")
+plt.ylabel("income")
+plt.scatter(df["year"],df["per capita income (US$)"],color='green',marker='*')
+# plt.show()
+d = df.drop("per capita income (US$)",axis='columns')
+model = linear_model.LinearRegression()
+model.fit(d,df["per capita income (US$)"],)
+print(model.predict([[2020]]))
---
+++ Machine_learning/2_Linear_Regression/linearReg.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+df = pd.read_csv('homeprices.csv')
+print(df)
+
+plt.xlabel('area')
+plt.ylabel('price')
+plt.scatter(df.area,df.price,color='red',marker='+')
+# plt.show()
+
+price = df.price
+print(price)
+area = df["area"]
+print(area)
+new_df = df.drop('price',axis='columns')
+print(new_df)
+reg = linear_model.LinearRegression()
+reg.fit(new_df,price)
+
+print(reg.predict([[2600]]))
+print(reg.coef_)
+print(reg.intercept_)
+
+area_df = pd.read_csv("areas.csv")
+p = reg.predict(area_df)
+area_df['price'] = p
+print(area_df)
+# area_df.to_csv("prediction.csv")
