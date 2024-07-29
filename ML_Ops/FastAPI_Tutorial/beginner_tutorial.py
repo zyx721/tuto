3c74e17555666b25851a353fcf6da6bd790ffa95
zyx721 <benmatiziad5@gmail.com>
2024-07-29 10:45:15 +0100

    update

--- ML_Ops/FastAPI_Tutorial/beginner_tutorial.py
+++ ML_Ops/FastAPI_Tutorial/beginner_tutorial.py
@@ -11,3 +11,13 @@ food_items = {
 @app.get("/get_items/{cuisine}")
 async def get_items(cuisine: availblecus):
     return food_items.get(cuisine)
+
+
+cc = {
+    1:'10%',
+    2:'20%',
+    3:'30%'
+}
+@app.get("/get_coupon/{c}")
+async def cc(c: int):
+    return {'discount_amount': cc.get(c)}
